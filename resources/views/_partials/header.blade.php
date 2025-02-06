b4dd63f48c7d658a79381dc7bfee04cd8cb753fe
Ryan Hayden <pastorryanhayden@gmail.com>
2025-02-06 13:58:22 -0600

    wip

--- resources/views/_partials/header.blade.php
+++ resources/views/_partials/header.blade.php
@@ -2,8 +2,8 @@
     <x-navigation />
     <div class="text">
         <h1>Ryan Hayden</h1>
-        <h3>Experienced <span>software generalist</span> looking<br>
-            for my next full-time software role.</h3>
+        <h3>Experienced<span>web generalist</span><br>
+            looking for my next full-time role.</h3>
 
         <a href="/resume.pdf" target="_blank" class="resume"><x-tabler-file-cv />Download Resume</a>
         <p>(or send me an email at <br>ryan.hayden@hey.com)</p>
